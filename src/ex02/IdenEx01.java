25de05947052a10e62a9c5ee34b766aafe92776f
yun <yunnn687@naver.com>
2023-12-13 12:40:48 +0900

    ãstatic 공부

---
+++ src/ex02/IdenEx01.java
@@ -0,0 +1,19 @@
+package ex02;
+
+/** 여러줄 적을때는 * 두 번
+ * 변수 작성법
+ * 1. 카멜(낙타) 표기법 사용
+ * 2. 숫자를 먼저 사용 x
+ * 3. 특수문자 사용 x
+ */
+public class IdenEx01 {
+    public static void main(String[] args) {
+        // int main; 키워드는 변수명으로 사용 x
+        int user_money; // xxx절대 사용 안 함!!xxx
+        int userMoney; // 낙타 표기법 (카멜 표기법)
+        // int abc#;
+        int method;
+        double abc$$; // 특수문자 사용하지 말자
+        // int 1stMoney; 첫글자로 숫자 x
+    }
+}
