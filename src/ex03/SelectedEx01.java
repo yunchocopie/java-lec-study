dcbbfa8ddfeb9708f909c3a0fae93c4cf3f4391d
yun <yunnn687@naver.com>
2023-12-18 10:41:09 +0900

    Selection Sort 공부

---
+++ src/ex03/SelectedEx01.java
@@ -0,0 +1,32 @@
+package ex03;
+
+public class SelectedEx01 {
+    public static void main(String[] args) {
+        int[] arr = {5, 8, 2, 4, 3};
+        final int N = arr.length;
+        int rep;
+        int min;
+
+        for (int j = 0; j < N-1; j++) {
+            rep = j;
+            min = rep;
+
+            for (int i = rep + 1; i < N; i++) {
+                if(arr[min] > arr[i]){
+                    min = i;
+                }
+            }
+
+            if(rep != min){
+                int temp = arr[rep];
+                arr[rep] = arr[min];
+                arr[min] = temp;
+            }
+        }
+
+        for (int v : arr)
+            System.out.print(v + " ");
+
+
+    }
+}
