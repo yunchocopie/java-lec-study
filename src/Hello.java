e3b62139ba7fd7ef15d04b1b4fb1bfd416f2adbb
yun <yunnn687@naver.com>
2024-01-02 09:30:45 +0900

    리팩토링

--- src/Hello.java
+++ src/Hello.java
@@ -1,10 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
         System.out.println("4");
         System.out.println("5");
-
     }
 }
